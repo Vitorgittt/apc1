8ee843e9f8b19ea2f95e25c5f3ba4b082e51c444
unknown <2612082035@ACADEMICO.IESB.BR>
2026-03-11 10:57:33 -0300

    adiciona codigos da aula02

---
+++ aulas/aula02/saida.c
@@ -0,0 +1,14 @@
+#include <stdio.h> //inclusão de biblioteca da linguagem
+
+int main() {
+    // 1. Entrada
+
+    // 2. Processamento
+
+    // 3. Saida
+
+    printf("Ola Mundo.\n");
+    printf("%i\n", 10);
+
+    return 0;
+}

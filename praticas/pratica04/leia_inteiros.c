f5fe42ff6e70d55ee68dcccc8065fd3cbaf8a2db
Vitorgittt <vitorcarrilhotnas@gmail.com>
2026-04-08 10:09:20 -0300

    feat: conclui pratica04. Fecha #4

---
+++ praticas/pratica04/leia_inteiros.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main () {    
+
+    int N1;
+    int N2;
+
+    printf ("Digite um numero: ");
+    scanf ("%i", &N1);
+
+    printf ("Digite outro numero: ");
+    scanf ("%i", &N2);
+    
+    printf ("Primeiro numero: %i\n", N1);
+    
+    printf ("Segundo numero: %i\n", N2);
+
+    return 0;    
+}
